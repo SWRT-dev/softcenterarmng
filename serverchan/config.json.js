adf0ae173d459ee2b69dfa7f2bc61224ca038670
Xu Zhaowei <paldier@hotmail.com>
2020-05-20 18:37:19 +0800

    update serverchan

--- serverchan/config.json.js
+++ serverchan/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.7",
-"md5":"c81d7fe332e8e8879d4e69ca17031eba",
+"version":"1.8",
+"md5":"3c4b4cd2af725cc2392d0249bee227e1",
 "home_url":"Module_serverchan.asp",
 "title":"ServerChan微信推送",
 "description":"从路由器推送状态及通知的工具",
-"build_date":"2020-03-26_18:32:35"
+"build_date":"2020-05-20_18:36:42"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -251,14 +251,14 @@
             "version": "1.4"
         }, 
         {
-            "build_date": "2020-03-26_18:32:35", 
+            "build_date": "2020-05-20_18:36:42", 
             "description": "从路由器推送状态及通知的工具", 
             "home_url": "Module_serverchan.asp", 
-            "md5": "c81d7fe332e8e8879d4e69ca17031eba", 
+            "md5": "3c4b4cd2af725cc2392d0249bee227e1", 
             "name": "serverchan", 
             "tar_url": "serverchan/serverchan.tar.gz", 
             "title": "ServerChan微信推送", 
-            "version": "1.7"
+            "version": "1.8"
         }, 
         {
             "build_date": "2019-07-24_10:04:58", 
