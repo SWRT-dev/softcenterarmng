b4162575c80f9e4aeb846b0503f7c89adce6d825
Xu Zhaowei <paldier@hotmail.com>
2020-09-23 13:28:15 +0800

    update kp

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.4.4",
-"md5":"4087e67218f402275530d0b3638ac55e",
+"version":"3.8.5.0",
+"md5":"481a1068504a0c490ef1af46b86ee944",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"去广告，没烦恼",
-"build_date":"2020-08-20_11:45:09"
+"build_date":"2020-09-23_13:27:44"
 }
---
+++ phddns/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"3.0.2",
+"md5":"3fd737c1a506792406c493201846c47c",
+"home_url":"Module_phddns.asp",
+"title":"花生壳3.0",
+"description":"花生壳内网版3.0 （phddns） 动态域名 内网穿透",
+"build_date":"2020-09-23_13:27:39"
+}
