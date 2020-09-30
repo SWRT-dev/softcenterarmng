842de04735306a0ebd481d02805510320ee28388
Xu Zhaowei <paldier@hotmail.com>
2020-09-30 19:49:36 +0800

    add filebrowser

---
+++ filebrowser/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0.2",
+"md5":"0e7b3f439419a18ad54412068276c28f",
+"home_url":"Module_filebrowser.asp",
+"title":"filebrowser",
+"description":"FileBrowser：您的可视化路由文件管理系统",
+"build_date":"2020-09-30_15:28:05"
+}
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -21,6 +21,16 @@
             "version": "2.2"
         }, 
         {
+            "build_date": "2020-09-30_15:28:05", 
+            "description": "FileBrowser：您的可视化路由文件管理系统", 
+            "home_url": "Module_filebrowser.asp", 
+            "md5": "0e7b3f439419a18ad54412068276c28f", 
+            "name": "filebrowser", 
+            "tar_url": "filebrowser/filebrowser.tar.gz", 
+            "title": "filebrowser", 
+            "version": "1.0.2"
+        }, 
+        {
             "build_date": "2020-08-20_11:38:30", 
             "description": "Cloudflare解析自动更新IP", 
             "home_url": "Module_cfddns.asp", 
