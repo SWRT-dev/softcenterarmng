925600cf481ccc4696f6adcb9c9cafd4ea1c11ba
Xu Zhaowei <paldier@hotmail.com>
2020-12-30 08:57:37 +0800

    update node

--- node/config.json.js
+++ node/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"0.0.1",
-"md5":"ba82d1b893864b1bcf71e8e47c0ebd39",
+"version":"0.0.2",
+"md5":"7ad8affb6e70ab0bc186b8da3e634604",
 "home_url":"Module_node.asp",
 "title":"node.js",
 "description":"node.js",
-"build_date":"2020-12-29_10:28:55"
+"build_date":"2020-12-30_08:57:28"
 }
