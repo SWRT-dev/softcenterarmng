385be33c11597ed8d4022b9ba2d9090b4430a68d
Xu Zhaowei <paldier@hotmail.com>
2021-01-13 16:43:36 +0800

    add zerotier

---
+++ zerotier/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.1",
+"md5":"826b2ba0fae251497c6eb7d318cf8476",
+"home_url":"Module_zerotier.asp",
+"title":"zerotier",
+"description":"分布式的虚拟以太网",
+"build_date":"2021-01-13_16:43:13"
+}
