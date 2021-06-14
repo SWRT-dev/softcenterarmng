e25bb3b1997873559554bee74833ebbb7417c57f
Xu Zhaowei <paldier@hotmail.com>
2021-06-14 21:53:23 +0800

    update kp

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.5.2",
-"md5":"5594acb6e687dc1d2a844642aa197ba5",
+"version":"3.8.5.3",
+"md5":"3945309140edd2100739c1998ad2ca89",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"去广告，没烦恼",
-"build_date":"2021-03-28_13:01:41"
+"build_date":"2021-06-14_21:53:19"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -71,14 +71,14 @@
             "version": "0.4"
         }, 
         {
-            "build_date": "2021-03-28_13:01:41", 
+            "build_date": "2021-06-14_21:53:19", 
             "description": "去广告，没烦恼", 
             "home_url": "Module_koolproxy.asp", 
-            "md5": "5594acb6e687dc1d2a844642aa197ba5", 
+            "md5": "3945309140edd2100739c1998ad2ca89", 
             "name": "koolproxy", 
             "tar_url": "koolproxy/koolproxy.tar.gz", 
             "title": "koolproxy", 
-            "version": "3.8.5.2"
+            "version": "3.8.5.3"
         }, 
         {
             "build_date": "2020-08-20_11:35:07", 
