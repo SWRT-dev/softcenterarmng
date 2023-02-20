12ce9e6a4ef7a1624a5cff9e54b2e357e9d72ed9
ZhaoWei Xu <paldier@hotmail.com>
2023-02-20 09:12:30 +0800

    repack adbyby

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -234,10 +234,10 @@
         }, 
         {
             "arch": "armng", 
-            "build_date": "2023-02-19_17:39:34", 
+            "build_date": "2023-02-20_09:12:16", 
             "description": "广告屏蔽大师 Plus 可以全面过滤各种横幅、弹窗、视频广告，同时阻止跟踪、隐私窃取及各种恶意网站", 
             "home_url": "Module_adbyby.asp", 
-            "md5": "eb5599ef0356489a56f4f31aaa5ad6c5", 
+            "md5": "a3a24565bc9be2eaaf5d1ad42ede3ff2", 
             "name": "adbyby", 
             "tar_url": "adbyby/adbyby.tar.gz", 
             "title": "广告屏蔽大师 Plus", 
