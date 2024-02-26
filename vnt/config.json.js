13410af40b2c4c35d89d89434fb59461b09fc5cd
ZhaoWei Xu <paldier@hotmail.com>
2024-02-26 09:47:06 +0800

    add vnt

---
+++ vnt/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"1.0",
+"md5":"302b0fa724cfc1cf3fd298308d9f1853",
+"arch":"armng",
+"home_url":"Module_vnt.asp",
+"title":"vnt",
+"description":"简便高效的异地组网、内网穿透工具",
+"build_date":"2024-02-26_09:46:38"
+}
